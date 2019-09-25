e29939e77fe1b0ac3adb15320b6685fba74287f4
unknown <ara19_i5@aod.am>
2019-09-25 23:42:26 +0400

    added chat styles

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,48 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
-import ruLocale from 'date-fns/locale/ru'
-import classNames from 'classnames'
-import checkedSvg from 'assets/img/checked.svg';
+import React from "react";
+import PropTypes from "prop-types";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import ruLocale from "date-fns/locale/ru";
+import classNames from "classnames";
+import checkedSvg from "assets/img/checked.svg";
+import noReadedSvg from "assets/img/noreaded.svg";
 
-import './Message.scss'
+import "./Message.scss";
 
-const Message = ({ avatar, user, text, date, isMe, isReaded }) => {
-    return (
-        <div className={classNames('message', { 'message--isme': isMe })}>
-            <div className='message__content'>
-                <img src={checkedSvg} alt="Checked icon" />
-                <div className="message__avatar">
-                   {isMe && isReaded &&(
-                        <img className='message__icon-readed'
-                         src={avatar}
-                          alt={`Avatar${user.fullname}`} />
-                   )}
-                </div>
-                <div className='message_info'>
-                    <div className='message__bubble'>
-                        <p className='message__text'>{text}</p>
-                    </div>
-                    <span className='message__date'>
-                        {formatDistanceToNow(new Date(date), { addSuffix: true, locale: ruLocale })}
-                    </span>
-                </div>
-            </div>
+const Message = ({ avatar, user, text, date, isMe, isReaded, attachments }) => {
+  return (
+    <div className={classNames("message", { "message--isme": isMe })}>
+      <div className="message__content">
+        {isMe && isReaded ? (
+          <img
+            className="message__icon-readed"
+            src={checkedSvg}
+            alt="Readed-icon"
+          />
+        ) : (
+          <img
+            className="message__icon-readed message__icon-readed--no"
+            src={noReadedSvg}
+            alt="No-readed-icon"
+          />
+        )}
+        <div className="message__avatar">
+          <img src={avatar} alt={`Avatar ${user.fullname}`} />
         </div>
-    );
-
-
+        <div className="message_info">
+          <div>
+            <div className="message__bubble">
+              <p className="message__text">{text}</p>
+            </div>
+            <div className="message__attachments">
+              {attachments &&
+                attachments.map((item, key) => (
+                  <div className="message__attachments-items" key={key}>
+                    <img src={item.url} alt={item.filename} />
+                  </div>
+                ))}
+            </div>
+            <span className="message__date">
+              {formatDistanceToNow(new Date(date), {
+                addSuffix: true,
+                locale: ruLocale
+              })}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 Message.defaultProps = {
-    user: {}
-}
-
-
-Message.propTypes = {
-    avatar: PropTypes.string,
-    text: PropTypes.string,
-    date: PropTypes.string,
-    user: PropTypes.object
+  user: {}
 };
 
-export default Message
+Message.propTypes = {
+  avatar: PropTypes.string,
+  text: PropTypes.string,
+  date: PropTypes.string,
+  user: PropTypes.object,
+  attachments: PropTypes.array
+};
+
+export default Message;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import { Message } from 'components'
+import React from "react";
+import { Message } from "components";
 
-import './Home.scss'
+import "./Home.scss";
 
 const Home = () => (
-    <section className='home'>
-        <Message
-            avatar='https://pickaface.net/gallery/avatar/20151205_194059_2696_Chat.png'
-            text="Hello friend , how are u ?"
-            date='Mon Sep 23 2019 23:30:00' />
+  <section className="home">
+    <Message
+      avatar="https://pickaface.net/gallery/avatar/20151205_194059_2696_Chat.png"
+      text="Hello friend , how are u ?"
+      date="Mon Sep 23 2019 23:30:00"
+      attachments={[
+        {
+          filename: "image.jpg",
+          url: "https://source.unsplash.com/100x100/?random=1&nature,water"
+        },
+        {
+          filename: "image.jpg",
+          url: "https://source.unsplash.com/100x100/?random=2&nature,water"
+        },
+        {
+          filename: "image.jpg",
+          url: "https://source.unsplash.com/100x100/?random=3&nature,water"
+        }
+      ]}
+    />
 
-        <Message
-            avatar='https://www.stickees.com/files/avatars/male-avatars/1697-andrew-sticker.png'
-            text="Hello , I'm fine u ?"
-            date='Mon Sep 23 2019 23:59:00'
-            isMe={true}
-            isReaded={true}
-        />
-    </section>
+    <Message
+      avatar="https://www.stickees.com/files/avatars/male-avatars/1697-andrew-sticker.png"
+      text="Hello , I'm fine u ?"
+      date="Mon Sep 23 2019 23:59:00"
+      isMe={true}
+      isReaded={true}
+    />
+  </section>
 );
 
-
-export default Home
+export default Home;
